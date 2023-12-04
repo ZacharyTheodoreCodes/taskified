24d28385a0dbc8a8ba6d1f7716b409910753a4e2
Zachary Theodore <zacharytheodore.zac@gmail.com>
2023-12-04 12:39:51 +0700

    feat: input box

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        "tilt-warp": ["Tilt Warp", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
