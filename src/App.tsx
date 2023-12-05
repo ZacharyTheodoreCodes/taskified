87b84736e64c87b478613f06a30babc66413f5e4
Zachary Theodore <zacharytheodore.zac@gmail.com>
2023-12-05 19:56:15 +0700

    feat: droppable

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import TaskInput from "./components/TaskInput";
 import { Task } from "./model";
 import TaskList from "./components/TaskList";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 const App: React.FC = () => {
   const [task, setTask] = useState<string>("");
@@ -17,23 +17,56 @@ const App: React.FC = () => {
     }
   };
 
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+    console.log(result);
+
+    if (
+      !destination ||
+      (destination.droppableId == source.droppableId &&
+        destination.index == source.index)
+    ) {
+      return;
+    }
+
+    let add;
+    let ongoing = tasks;
+    let finished = FinishedTasks;
+    //source logic
+    if (source.droppableId == "ongoingTask") {
+      add = ongoing[source.index];
+      ongoing.splice(source.index, 1);
+    } else {
+      add = finished[source.index];
+      finished.splice(source.index, 1);
+    }
+
+    //destination logic
+    if (destination.droppableId == "ongoingTask") {
+      ongoing.splice(destination.index, 0, add);
+    } else {
+      finished.splice(destination.index, 0, add);
+    }
+
+    setFinishedTasks(finished);
+    setTasks(ongoing);
+  };
+
   return (
-    <>
-      <DragDropContext onDragEnd={() => {}}>
-        <div className="min-h-screen flex flex-col items-center font-raleway bg-gradient-to-r from-[#FFF78A] to-[#fee2e2]">
-          <h1 className="text-[50px] text-[#71717a] tracking-wider my-10 font-tilt-warp">
-            Taskified 📝
-          </h1>
-          <TaskInput task={task} setTask={setTask} handleAdd={handleAdd} />
-          <TaskList
-            tasks={tasks}
-            setTasks={setTasks}
-            FinishedTasks={FinishedTasks}
-            setFinishedTasks={setFinishedTasks}
-          />
-        </div>
-      </DragDropContext>
-    </>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="min-h-screen flex flex-col items-center font-raleway bg-gradient-to-r from-[#FFF78A] to-[#fee2e2]">
+        <h1 className="text-[50px] text-[#71717a] tracking-wider my-10 font-tilt-warp">
+          Taskified 📝
+        </h1>
+        <TaskInput task={task} setTask={setTask} handleAdd={handleAdd} />
+        <TaskList
+          tasks={tasks}
+          setTasks={setTasks}
+          FinishedTasks={FinishedTasks}
+          setFinishedTasks={setFinishedTasks}
+        />
+      </div>
+    </DragDropContext>
   );
 };
 
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,58 +15,55 @@ const TaskList = ({
   FinishedTasks,
   setFinishedTasks,
 }: Props) => {
-  
   return (
-    <>
-      <div className="w-[60%] grid grid-cols-2 gap-2 justify-between items-start">
-        <Droppable droppableId="ongoingTask">
-          {(provided) => (
-            <div
-              className="bg-[#6DB9EF] rounded-lg p-5"
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              <p className="text-[24px] text-white font-bold mb-4">
-                Ongoing Task
-              </p>
-              {tasks?.map((task, index) => (
-                <SingleTask
-                  index={index}
-                  task={task}
-                  key={task.id}
-                  tasks={tasks}
-                  setTasks={setTasks}
-                />
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-        <Droppable droppableId="completedTask">
-          {(provided) => (
-            <div
-              className="bg-[#FF8F8F] rounded-lg p-5"
-              ref={provided.innerRef}
-              {...provided.droppableProps}
-            >
-              <p className="text-[24px] text-white font-bold mb-4">
-                Finished Task
-              </p>
-              {FinishedTasks?.map((task, index) => (
-                <SingleTask
-                  index={index}
-                  task={task}
-                  key={task.id}
-                  tasks={FinishedTasks}
-                  setTasks={setFinishedTasks}
-                />
-              ))}
-              {provided.placeholder}
-            </div>
-          )}
-        </Droppable>
-      </div>
-    </>
+    <div className="w-[60%] grid grid-cols-2 gap-2 justify-between items-start">
+      <Droppable droppableId="ongoingTask">
+        {(provided) => (
+          <div
+            className="bg-[#6DB9EF] rounded-lg p-5"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <p className="text-[24px] text-white font-bold mb-4">
+              Ongoing Task
+            </p>
+            {tasks?.map((task, index) => (
+              <SingleTask
+                index={index}
+                task={task}
+                key={task.id}
+                tasks={tasks}
+                setTasks={setTasks}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+      <Droppable droppableId="completedTask">
+        {(provided) => (
+          <div
+            className="bg-[#FF8F8F] rounded-lg p-5"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <p className="text-[24px] text-white font-bold mb-4">
+              Finished Task
+            </p>
+            {FinishedTasks?.map((task, index) => (
+              <SingleTask
+                index={index}
+                task={task}
+                key={task.id}
+                tasks={FinishedTasks}
+                setTasks={setFinishedTasks}
+              />
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </div>
   );
 };
 
